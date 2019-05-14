3efd04089553e3eb694b2204240b68cfb208fdf0
Chase <chase.gilliam@gmail.com>
2019-05-13 22:45:48 -0400

    get versions aligned

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Raining.MixProject do
     [
       app: :raining,
       version: "0.1.0",
-      elixir: "~> 1.8.2",
+      elixir: "~> 1.8.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
