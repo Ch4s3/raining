47e8630173f42abda821e42a22d1c76fbc4afbfd
Chase <chase.gilliam@gmail.com>
2019-05-16 10:01:50 -0400

    try installing ruvy this way

---
+++ scripts/install_ruby.sh
@@ -0,0 +1,17 @@
+sudo apt-get update 
+sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+rbenv install 2.5.1
+rbenv global 2.5.1
+ruby -v
+sudo apt-get install rubygems
+gem install sendgrid-ruby
