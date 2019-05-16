c6f265f0c204d59a0eac2b6e0e15f618d15d5c98
Chase <chase.gilliam@gmail.com>
2019-05-16 13:11:06 -0400

    try again

--- config/config.exs
+++ config/config.exs
@@ -48,7 +48,6 @@ config :darkskyx,
     lang: "en"
   ]
 import_config "scout_apm.exs"
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
