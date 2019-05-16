37aa28c087d32c70200fea5959bc7a67efe729f5
Chase <chase.gilliam@gmail.com>
2019-05-16 11:34:29 -0400

    fix rbenv maybe?

--- scripts/install_ruby.sh
+++ scripts/install_ruby.sh
@@ -5,18 +5,18 @@ sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison b
 echo "Installing Rbenv"
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
-
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 echo "Installing Ruby"
 rbenv install 2.5.1
 rbenv global 2.5.1
-ruby -v
+rbenv local --unset
+rbenv shell --unset
+echo `which ruby`
 sudo apt-get install rubygems
 echo "Installing SendGrid"
 gem install sendgrid-ruby
