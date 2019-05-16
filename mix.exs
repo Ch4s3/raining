e670754d2ed19839dcce3fba53c5226c33ce08d1
Chase <chase.gilliam@gmail.com>
2019-05-15 23:37:21 -0400

    add docs to test

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule Raining.MixProject do
       {:sobelow, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:sentry, "~> 7.0"},
       {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false}
     ]
   end
 
