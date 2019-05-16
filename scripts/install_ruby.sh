8347dd7b36fe87f0fee19f650197a4037bee4036
Chase <chase.gilliam@gmail.com>
2019-05-16 11:21:08 -0400

    fix script?

--- scripts/install_ruby.sh
+++ scripts/install_ruby.sh
@@ -7,11 +7,11 @@ echo "Installing Rbenv"
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+source ~/.bashrc
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+source ~/.bashrc
 
 echo "Installing Ruby"
 rbenv install 2.5.1
