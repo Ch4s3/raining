20e55d8ae7a0b472fbcc0e0497bf9d7701af33dd
Chase <chase.gilliam@gmail.com>
2019-05-16 11:52:56 -0400

    fix endpoint

--- scripts/install_ruby.sh
+++ scripts/install_ruby.sh
@@ -3,12 +3,9 @@ sudo apt-get update
 sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
 
 echo "Installing Rbenv"
-
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 echo "Installing Ruby"
@@ -17,7 +14,7 @@ rbenv global 2.5.1
 rbenv local --unset
 rbenv shell --unset
 echo `which ruby`
-sudo apt-get install rubygems
+echo "gem: --no-document" > ~/.gemrc
 echo "Installing SendGrid"
 gem install sendgrid-ruby
 
